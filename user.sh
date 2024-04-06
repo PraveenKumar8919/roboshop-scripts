c35e419a12cbbb4798bf78400a7d76e14be14bdb
PraveenKumar8919 <ppattiri@aum.edu>
2024-04-06 17:24:15 -0500

    changes

--- user.sh
+++ user.sh
@@ -74,7 +74,7 @@ VALIDATE $? "Enable user"
 systemctl start user &>> $LOGFILE
 VALIDATE $? "Starting user"
 
-cp /home/centos/roboshop-scripts/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-scripts/mongo.repo /etc/yum.repos.d/mongo.repo #copying 
 VALIDATE $? "copying mongodb repo"
 
 dnf install mongodb-org-shell -y &>> $LOGFILE
